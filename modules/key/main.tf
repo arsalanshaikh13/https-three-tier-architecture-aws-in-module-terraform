768be4f1a2b63acb5b73173d3752849ab1672ef7
arsalanshaikh13 <99062652+arsalanshaikh13@users.noreply.github.com>
2025-12-14 21:49:18 +0530

    Rename key_name from 'client_key' to 'server_key'

--- modules/key/main.tf
+++ modules/key/main.tf
@@ -3,6 +3,6 @@ resource "aws_key_pair" "client_key" {
     public_key = file("../modules/key/client_key.pub")
 }
 resource "aws_key_pair" "server_key" {
-    key_name = "client_key"
+    key_name = "server_key"
     public_key = file("../modules/key/server_key.pub")
 }
